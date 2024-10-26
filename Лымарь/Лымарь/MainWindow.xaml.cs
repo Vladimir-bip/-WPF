1d9d0f2ee87f7c3033f064269cfc11756ca84cf1
Liana <midatovaliana@gmail.com>
2024-10-26 09:12:55 +0500

    111

---
+++ Лымарь/Лымарь/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Лымарь
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Menu_123.Background = new SolidColorBrush(Colors.Blue);
+            Menu_1.Background = new SolidColorBrush(Colors.Blue);
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Автор: Владимир Лымарь", "Авторы", 0 , MessageBoxImage.Information);
+        }
+
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void mi_open_MouseEnter(object sender, MouseEventArgs e)
+        {
+            rr1.Text = "Изменить цвет";
+        }
+
+        private void MenuItem_MouseEnter(object sender, MouseEventArgs e)
+        {
+            rr1.Text = "Авторы";
+        }
+
+        private void MenuItem_MouseEnter_1(object sender, MouseEventArgs e)
+        {
+            rr1.Text = "Закрыть меню";
+        }
+    }
+}
